dbe859c46ab0bd9baeb4d17c7ca18492f4080d68
Yang Jiang <jiangyang.jnu@gmail.com>
2023-06-16 16:22:15 -0700

    add function to view other users profile

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Profile from "@components/Profile";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+
+
+const UserProfile = ({params}) => {
+  const searchParams = useSearchParams();
+  const username = searchParams.get('name');
+  console.log(params.id);
+
+  const [userPosts, setUserPosts] = useState([]);
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+
+      setUserPosts(data);
+    }
+    if(params?.id) {
+      fetchPosts();
+    } 
+  }, [params.id]);
+  
+  return (
+    <Profile 
+      name={username}
+      desc={`Welcome to ${username} profile page`}
+      data={userPosts}
+    />
+  )
+}
+
+export default UserProfile
