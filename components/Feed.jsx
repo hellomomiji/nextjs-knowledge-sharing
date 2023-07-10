1e6fe2cc1a198aa13718948a4da7a98ae2b9e95d
Yang Jiang <jiangyang.jnu@gmail.com>
2023-07-09 23:27:47 -0700

    update cache config again

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -25,7 +25,7 @@ const Feed = () => {
   const [searchResults, setSearchResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch('/api/post');
+    const response = await fetch('/api/post', {cache: 'no-store'});
     const data = await response.json();
     setAllPosts(data);
   };
