a958b580c63c92f0ef6a3e6d21555d682364d80d
Yang Jiang <jiangyang.jnu@gmail.com>
2023-07-09 23:32:48 -0700

    add revalidatePath in route.js in post

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,11 +1,13 @@
 import { connectToDB } from "@utils/database";
 import Post from "@models/post";
+import { revalidatePath } from "next/cache";
 
 export const GET = async (req) => {
   try {
     await connectToDB();
     const posts = await Post.find({}).populate('creator');
     
+    revalidatePath('/api/post');
     return new Response(JSON.stringify(posts), {status: 200});
 
   } catch (error) {
